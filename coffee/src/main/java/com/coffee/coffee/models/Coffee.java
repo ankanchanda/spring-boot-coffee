c35a73ac1ed62f988dc07883e3377504ca905599
Ankan Chanda <ankanchanda@Ankans-MacBook-Air.local>
2024-03-13 20:30:06 +0530

    models

---
+++ coffee/src/main/java/com/coffee/coffee/models/Coffee.java
@@ -0,0 +1,30 @@
+package com.coffee.coffee.models;
+
+import java.util.UUID;
+
+public class Coffee {
+    
+    private final String id;
+    private String name;
+
+    Coffee(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    Coffee(String name){
+        this(UUID.randomUUID().toString(), name);
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
